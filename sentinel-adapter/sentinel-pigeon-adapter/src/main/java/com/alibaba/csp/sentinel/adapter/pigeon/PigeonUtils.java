a5af3c3b9eaa16bd41fe60855d67ce6357a40fc6
Jobs Wang <395357470@qq.com>
2019-05-21 19:28:04 +0800

    add pigeon adapter

--- sentinel-adapter/sentinel-pigeon-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/pigeon/PigeonUtils.java
+++ sentinel-adapter/sentinel-pigeon-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/pigeon/PigeonUtils.java
@@ -1,40 +1,24 @@
 package com.alibaba.csp.sentinel.adapter.pigeon;
 
-import com.dianping.pigeon.remoting.invoker.domain.InvokerContext;
+import com.dianping.pigeon.remoting.common.domain.InvocationContext;
 import com.dianping.pigeon.remoting.provider.domain.ProviderContext;
 
 import java.lang.reflect.Parameter;
 
 public final class PigeonUtils {
 
-    public static String getResourceName(ProviderContext providerContext) {
+    public static String getResourceName(InvocationContext context) {
         StringBuilder buf = new StringBuilder(64);
-        buf.append(providerContext.getMethodUri())
-            .append("(");
-        boolean isFirst = true;
-        for (Class<?> clazz : providerContext.getServiceMethod().getMethod().getParameterTypes()) {
-            if (!isFirst) {
-                buf.append(",");
-            }
-            buf.append(clazz.getName());
-            isFirst = false;
-        }
-        buf.append(")");
-        return buf.toString();
-    }
-
-    public static String getResourceName(InvokerContext invokerContext) {
-        StringBuilder buf = new StringBuilder(64);
-        buf.append(invokerContext.getClient().getConnectInfo().getConnect())
+        buf.append(context.getRequest().getServiceName())
                 .append(":")
-                .append(invokerContext.getMethodName())
+                .append(context.getRequest().getMethodName())
                 .append("(");
         boolean isFirst = true;
-        for (Class<?> clazz : invokerContext.getParameterTypes()) {
+        for (String clazz : context.getRequest().getParamClassName()) {
             if (!isFirst) {
                 buf.append(",");
             }
-            buf.append(clazz.getName());
+            buf.append(clazz);
             isFirst = false;
         }
         buf.append(")");
